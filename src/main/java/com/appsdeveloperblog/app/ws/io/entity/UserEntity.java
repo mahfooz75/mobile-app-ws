402258ba78ee06bc489b71fafa2629918f4e8a7d
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-04-27 23:41:41 +0530

    Preventing duplicate entries with @Column(unique=true)

--- src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -26,7 +26,7 @@ public class UserEntity implements Serializable {
 	@Column(nullable = false, length = 50)
 	private String lastName;
 
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false, length = 120, unique = true)
 	private String email;
 
 	@Column(nullable = false)
