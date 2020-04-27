331371c1cc42ec5d7964bb16c8d383150337a534
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-04-28 00:09:58 +0530

    Generate User Public ID

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Service;
 import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private Utils utils;
 	
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -25,7 +29,7 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(user, userEntity);
 		
 		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
+		userEntity.setUserId(utils.generateUserId(30));
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		UserDto returnValue = new UserDto();
